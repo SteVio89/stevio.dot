24858c5d0c3b916ead58bd3ce03c909c21a2cfc1
Stefan Viol <stefan@stevio.de>
2025-04-14 23:15:07 +0200

    add dependency to files

--- .config/nvim/lua/plugins/mini_files.lua
+++ .config/nvim/lua/plugins/mini_files.lua
@@ -1,24 +1,26 @@
 return {
     "echasnovski/mini.files",
     version = '*',
+    dependencies = {
+        { "echasnovski/mini.icons", version = "*" },
+    },
     config = function()
         require("mini.files").setup({
             windows = {
                 preview = true
             },
             options = {
-                use_as_default_explorer = false,
+                use_as_default_explorer = true,
             }
         })
-
         vim.keymap.set("n", "<leader>fe", function()
             local buffer_name = vim.api.nvim_buf_get_name(0)
             if buffer_name == "" or string.match(buffer_name, "Starter") then
-                require('mini.files').open(vim.loop.cwd())
+                require("mini.files").open(vim.loop.cwd())
             else
-                require('mini.files').open(vim.api.nvim_buf_get_name(0))
+                require("mini.files").open(vim.api.nvim_buf_get_name(0))
             end
-        end, { desc = 'Open file explorer' })
+        end, { desc = "Open file explorer" })
     end,
     init = function()
         vim.g.loaded_netrw = 1
