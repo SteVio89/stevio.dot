8cb779b12e98c364351b18a46186f90599e9f061
Stefan Viol <stefan@stevio.de>
2025-06-03 11:25:43 +0200

    more telescope, remove trouble

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -12,5 +12,12 @@ return {
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep files" })
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffer" })
 		vim.keymap.set("n", "<leader>fu", "<cmd>Telescope undo<cr>", { desc = "Undo history" })
+		vim.keymap.set("n", "<leader>fd", function()
+			builtin.diagnostics({ bufnr = 0 })
+		end, { desc = "Diagnostics this buffer" })
+		vim.keymap.set("n", "<leader>fD", builtin.diagnostics, { desc = "Diagnostics all buffers" })
+    vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "Reference under the cursor" })
+		vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Symbols this buffer" })
+		vim.keymap.set("n", "<leader>fS", builtin.lsp_workspace_symbols, { desc = "Symbols alls buffers" })
 	end,
 }
