c649f9987cad64c5278b3239f70de9e5561a5936
Stefan Viol <stefan@stevio.de>
2026-04-25 00:50:24 +0200

    switch to start-hyprland

--- .config/nix/hosts/kids-laptop/default.nix
+++ .config/nix/hosts/kids-laptop/default.nix
@@ -60,7 +60,7 @@
     waylandCompositors.hyprland = {
       prettyName = "Hyprland";
       comment    = "Hyprland managed by UWSM";
-      binPath    = "/run/current-system/sw/bin/Hyprland";
+      binPath    = "/run/current-system/sw/bin/start-hyprland";
     };
   };
 
